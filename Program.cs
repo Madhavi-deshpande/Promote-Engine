efac414efd2fb6099ddaf20bd7a030788f5abb08
Madhavi-deshpande <deshpande.v.madhavi@gmail.com>
2022-04-13 18:22:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using PromotionEngine.BusinessLayer;
+using PromotionEngine.Infrastructure;
+using System;
+
+namespace PromotionEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LogWriter.LogWrite("Promotion Engine is initialized : ");
+
+                Facade facade = new Facade();
+
+
+                facade.CheckoutProducts();///User input for the Product quantity  
+
+                facade.ApplyPromotion();// Apply Promotion offers for the product
+
+                facade.DisplayTotalPrice(); //Display result 
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                LogWriter.LogWrite("Exception in Promotion Engine .... : " + ex.Message);
+            }
+        }
+    }
+}
